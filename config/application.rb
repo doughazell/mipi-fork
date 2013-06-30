c13a7afeb8afda51d8ecf047c93da89a8d62ac37
Paul Long <paul_a_long@hotmail.com>
2013-06-30 21:02:31 +0100

    Efforts to get Asset Pipeline working with images.
    Signed-off-by: Paul Long <paul_a_long@hotmail.com>

--- config/application.rb
+++ config/application.rb
@@ -53,5 +53,7 @@ module V01
     
     # Change this if you want to expire all your assets.
     config.assets.version = '1.0'
+    
+    config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif] 
   end
 end
