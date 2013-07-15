81c624420b599e478764dfbddd4b862211093e0c
Doug Hazell <doughazell@gmail.com>
2013-07-15 22:52:32 +0100

    Engine Yard deploy bug - Take 11

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-require 'liquid'
-require 'rails_sql_views'
+
+#require 'liquid'
+#require 'rails_sql_views'
 
 # Initialize the rails application
 V01::Application.initialize!
