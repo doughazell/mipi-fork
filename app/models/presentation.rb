58f653f72e97428d66457025858b01a1456d3eed
Paul Long <paul_a_long@hotmail.com>
2013-07-29 12:05:47 +0100

    Resolved problematic self referencing confusing the class type.
    Signed-off-by: Paul Long <paul_a_long@hotmail.com>

--- app/models/presentation.rb
+++ app/models/presentation.rb
@@ -6,10 +6,13 @@ class Presentation < ActiveRecord::Base
   
   attr_accessible :id, :data_sheet_id, :data_element_collection_id, :created_at, :updated_at
   
+  # Calling Presentation again from class method seems to be problematic, creating
+  # a call to ActsAsTree. Explicit Presentation reference removed.
   def self.find_other_data_sheets(data_element_collection_id, sheet)
-    all = Presentation.find(:all, :conditions => {:data_element_collection_id => data_element_collection_id})
-    if all.include?(Presentation.find_by_data_sheet_id_and_data_element_collection_id(sheet.id, data_element_collection_id)) then
-      all.delete(Presentation.find_by_data_sheet_id_and_data_element_collection_id(sheet.id, data_element_collection_id))
+#    all = Presentation.find(:all, :conditions => {:data_element_collection_id => data_element_collection_id})
+    all = find(:all, :conditions => {:data_element_collection_id => data_element_collection_id})
+    if all.include?(find_by_data_sheet_id_and_data_element_collection_id(sheet.id, data_element_collection_id)) then
+      all.delete(find_by_data_sheet_id_and_data_element_collection_id(sheet.id, data_element_collection_id))
     end
     all
   end
