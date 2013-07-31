5a1dd5ac76e57826ea3d3ae23585a7bfb030074f
Doug Hazell <doughazell@gmail.com>
2013-07-31 12:03:26 +0100

    Preparing to do a Pull Request on selected files after sig number of commits

--- config/initializers/ey.rb
+++ config/initializers/ey.rb
@@ -1,1 +1,2 @@
+# 31/7/13 DH: Necessary to load the Engine Yard allocated IP address, as staging to correcting an invalid subdomain.
 EY_CONFIG = YAML.load_file("#{Rails.root}/config/ey.yml")[Rails.env]
